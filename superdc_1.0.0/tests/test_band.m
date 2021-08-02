787bf6df35d329f3ee73ca479101b9848720371d
fastsolvers <88216409+fastsolvers@users.noreply.github.com>
2021-08-02 18:06:22 -0400

    Update test_band.m

--- superdc_1.0.0/tests/test_band.m
+++ superdc_1.0.0/tests/test_band.m
@@ -3,15 +3,20 @@
 addpath(genpath('../../superdc_1.0.0'))
 addpath(genpath('../../fmm1d'))
 
-%%% Banded matrix example
-n = 4096;  % matrix size
-w = 1;     % half bandwidth
-A = 3*eye(n)-diag(ones(n-1,1),1)-diag(ones(n-1,1),-1);
+%%% Random banded matrix example
+n = 8192;  % matrix size
+w = 2;     % half bandwidth
 
+A = diag(rand(n,1));
+for i = 1:w
+    v = rand(n-w,1);
+    A = A+diag(v,w)+diag(v,-w);
+end
+
+%%% band2hss
 m0 = 2^11;  % HSS leaf level diagonal block size
 [tr,m] = npart(n,m0); % HSS partition info
 
-%%% band2hss
 [D,U,R,B] = band2hss(A,w,tr,m);
 
 %%% SuperDC eigensolver
@@ -19,25 +24,31 @@ tol = 1e-10;
 accchk = 1;
 
 fprintf('SuperDC ...\n')
+
 tic
-[d,Q,time_divide,flops_divide,time_conquer,flops_conquer,rho] = superdc(D,U,B,R,tr,tol);
+[d,Q] = superdc(D,U,B,R,tr,tol);
 tsuperdc = toc;
+
 fprintf('SuperDC     time: %11.4e\n',tsuperdc);
 
 %%% Application of the eigenmatrix to vectors.
 x = randn(n,1);
+
 tic
-[y,nflops] = superdcmv(Q,x,0);
+y = superdcmv(Q,x,0);
 tmv = toc;
+
 fprintf('Q mat-vec   time: %11.4e\n',tmv);
 
 %%% SuperDC accuracy check ( decomposition residual,  loss of orthogonality )
 if accchk == 1    
     % MATLAB eig
     fprintf('\nMatlab eig ...\n')
+    
     tic
     [~,D0] = eig(A);
     teig = toc;
+    
     fprintf('eig         time: %11.4e\n',teig);
     
     d0 = diag(D0);
